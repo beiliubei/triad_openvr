6655835c69f17e738a3700246beb2e9e581ec5cd
beiliubei <beiliubei@gmail.com>
2018-02-24 13:07:38 +0800

    add server

--- controller_test.py
+++ controller_test.py
@@ -2,25 +2,42 @@ import triad_openvr
 import time
 import sys
 
+import socket
+
+HOST = '192.168.1.61'
+PORT = 8001
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(5)
+
+print 'Server start at: %s:%s' % (HOST, PORT)
+print 'wait for connection...'
+
 v = triad_openvr.triad_openvr()
 v.print_discovered_objects()
 
 if len(sys.argv) == 1:
-    interval = 1/250
+    interval = 1 / 250
 elif len(sys.argv) == 2:
-    interval = 1/float(sys.argv[0])
+    interval = 1 / float(sys.argv[0])
 else:
     print("Invalid number of arguments")
     interval = False
-    
-if interval:
-    while(True):
-        start = time.time()
-        txt = ""
-        for each in v.devices["controller_1"].get_pose_euler():
-            txt += "%.4f" % each
-            txt += " "
-        print("\r" + txt, end="")
-        sleep_time = interval-(time.time()-start)
-        if sleep_time>0:
-            time.sleep(sleep_time)
+
+conn, addr = s.accept()
+print 'Connected by ', addr
+
+while (True):
+    start = time.time()
+    txt = ''
+    for each in v.devices["controller_1"].get_pose_euler():
+        txt += "%.4f" % each
+        txt += " "
+    print("\r" + txt)
+
+    conn.send("\r" + txt)
+
+    sleep_time = interval - (time.time() - start)
+    if sleep_time > 0:
+        time.sleep(sleep_time)
